a2516c54c248bc45c9f9126b1805feefdf8f2a52
MadPapaya <weekofkex@gmail.com>
2021-03-05 16:20:12 +0300

    init: upload TAP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-exports.min = function min (array) {
-    return (array && array.length) ? Math.min.apply(null, array) : 0
-}
-exports.max = function max (array) {
-    return (array && array.length) ?  Math.max.apply(null, array)  : 0
+exports.min = function min (arr) {
+    return (arr && arr.length) ? Math.min.apply(0, arr) : 0
 };
-exports.avg = function avg (array) {
-    return (array && array.length) ? array.reduce((accumulator, number) => (accumulator) + (number)) / array.length : 0
+
+exports.max = function max (arr) {
+    return (arr && arr.length) ? Math.max.apply(0, arr) : 0
+};
+
+exports.avg = function avg (arr) {
+    return (arr && arr.length) ? arr.reduce((accumulator, number) => accumulator + number) / arr.length : 0
 };
